a3341a5eae27fdfb9ca385e3148900e00778ba41
Gastonm1 <gaston.marques@gmail.com>
2019-02-23 07:53:35 -0600

    update

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-// ================INDEX.JS FUNCTION OF GAME=================================================
+// =========================INDEX.JS FUNCTION OF GAME========================================
 // Create the Word bank (THEME = programming languages) DONE
 // Have a start game function DONE
 // Inside game function, pull a word from the bank to transform into underscore DONE
 // Once you have the word, display the underscores DONE
 // Ask the user for a input
 //===========================================================================================
-
-
+//=============================GLOBAL VARS===================================================
 var inquirer = require("inquirer");
 var Word = require("./Word.js");
 var wordBank = ["java", "basic", "swift", "ruby", "c", "typescript", "c#", "c++", "php", 'python', 'javascript', 'matlab', 'assembly', 'perl', 'delphi', 'go', 'sql'];
 var guesses = 5;
+var randomWordfromBank = wordBank[Math.floor(Math.random() * wordBank.length)];
+var newWord = new Word(randomWordfromBank);
 
+//===========================================================================================
+//=============="And here we go - Joker"=====================================================
 
 function gameStart() {
-  var randomWordfromBank =
-    wordBank[Math.floor(Math.random() * wordBank.length)];
-  var newWord = new Word(randomWordfromBank);
+  
+  console.log("Welcome to Hangman in the terminal! You will have 5 guesses to guess the correct word! Please find today's category below! Have fun!")
   console.log("Category: ~~~~~Programming Languages~~~~~")
   console.log("You have " + guesses + " guesses!")
   console.log(newWord.underscores());
@@ -26,9 +28,32 @@ function gameStart() {
 
 function givemealetter (input){
 
-    inquirer.prompt([{type: "input", name: "letterInput", message: "Guess the letter!"}]).then(answers => { 
+    inquirer.prompt([{type: "input", name: "You guessed:", message: "Guess the letter!"}]).then(answers => { 
       console.log(answers)
+      if(answers = newWord){
 
+      }
+    
+      console.log("You have " + guesses + " guesses left! Try Again!")
+      if (guesses <= 0){
+        console.log("~~~~~~~~~~~~~~~~~~~~~")
+        console.log("Game Over :(")
+        console.log("The langauge was: " + randomWordfromBank);
+        console.log("~~~~~~~~~~~~~~~~~~~~~")
+        inquirer.prompt([{type: "confirm", name: "Confirm:", message: "Do you want to play again?", default: true}]).then(function(again){
+          if(again.confirm == false){
+            return false;
+          } else{
+            gameStart();
+          }
+        })
+      }
+      // if (input === newWord.length){
+      //   guesses = -1
+      //   console.log("Try again!")
+      // } else{
+      //   this.vis = true;
+      // }
       
       // In here: Have the inqurier or the prompt outside the game start to run over and over again, depending on the answer. If wrong answer, ask again, if it is the right answer the toggle the visibilty of letter.
         
